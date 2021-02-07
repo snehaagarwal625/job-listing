2f160fd2d1af4cea720c285aebd26de3bb14488f
“Bitupon <sachin.bitupon@gmail.com>
2021-02-07 13:44:15 +0530

    added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -12,6 +13,10 @@ const routes: Routes = [
     {
       path: 'login',
       component: LoginComponent
+    }
+    {
+      path:'dashboard',
+      component:DashboardComponent
     }
 ];
 
